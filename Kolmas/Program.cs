0b4dacfb3b775c4c9790cf02be35bb97425da2b0
elipri <Ely@Kasutajas-MBP.lan>
2020-11-08 21:28:06 +0200

    Removed description.

--- Kolmas/Program.cs
+++ Kolmas/Program.cs
@@ -6,7 +6,6 @@ namespace Kolmas
     {
         static void Main(string[] args)
         {
-            //3.Loome pisikese mängu, kus kas vastane või arvuti paneb esmalt kirja numbri vahemikus 1..50 ja seejärel püüame pakutud arvu ära arvata.Vastusena igale pakkumisele peame ütlema, kas tegu on pakutud arvuga, on see väiksem kui pakutu või suurem kui pakutu.Kui tühi string pannakse, katkestatakse mäng.Lõpuks teavitatakse mitu katset on tehtud ja kas õige tulemus leiti või mitte.
             int inimene;
             int arvuti;
             int katsed = 0;
